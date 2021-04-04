0dc47d45a8a317d44c0de80449b5416fb6dc6106
kobakobashu <kobaya4@fuji.waseda.jp>
2021-04-04 18:27:53 +0900

    change_code, write_complexity

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -5,13 +5,31 @@
 #
 
 # @lc code=start
+"""
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        time: O(n**2)
+        space: O(n**2)
+        
         n = len(nums)
         for i in range(n):
             ans = target - nums[i]
             for j in range(i+1, n):
                 if ans == nums[j]:
                     return [i,j]
-        
+"""
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        time: O(n)
+        space: O(n)
+        """
+        if nums is None or target is None:
+            return None
+        ref = {}
+        for idx, num in enumerate(nums):
+            if target - num in ref:
+                return [ref[target - num], idx]
+            ref[num] = idx
 # @lc code=end
