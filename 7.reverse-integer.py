f8781e607b9fab3a57b9d84863bd7a583ac42a13
kobakobashu <kobaya4@fuji.waseda.jp>
2021-04-05 23:12:03 +0900

    fix

--- 7.reverse-integer.py
+++ 7.reverse-integer.py
@@ -8,16 +8,16 @@
 class Solution:
     def reverse(self, x: int) -> int:
         """
-        time: O(log x)
-        space: O(1)
+        time: O(len(x))
+        space: O(len(x))
         """
         if x >= 0:
             x = str(x)
             output = int(x[::-1])
         else:
-            x = str(-1*x)
+            x = str(-x)
             x = int(x[::-1])
-            output = -1 * x
+            output = -x
         if output > 2 ** 31 or output < -1 * 2 ** 31:
             return 0
         return output
