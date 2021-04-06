7151feabb298a455e9a8fbcc9f85094594a2cc02
kobakobashu <kobaya4@fuji.waseda.jp>
2021-04-06 10:49:12 +0900

    palindrome-number

---
+++ 9.palindrome-number.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=9 lang=python3
+#
+# [9] Palindrome Number
+#
+
+# @lc code=start
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if 2 ** 31 < abs(x):
+            return 0
+        else:
+            x = str(x)
+            output = (x)[::-1]
+            if x == output:
+                return True
+            return False
+
+# @lc code=end
+
