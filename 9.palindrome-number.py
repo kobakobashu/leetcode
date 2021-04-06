1ac2a5b023fa051271af92fd7c3674064da4b65e
kobakobashu <kobaya4@fuji.waseda.jp>
2021-04-06 10:52:31 +0900

    complexity

--- 9.palindrome-number.py
+++ 9.palindrome-number.py
@@ -7,6 +7,10 @@
 # @lc code=start
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        """
+        time: O(len(x))
+        space: O(len(x))
+        """
         if 2 ** 31 < abs(x):
             return 0
         else:
