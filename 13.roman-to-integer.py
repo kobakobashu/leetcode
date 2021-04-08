90b845dcd48457d9fc236ed74dd5d27658106573
kobakobashu <kobaya4@fuji.waseda.jp>
2021-04-08 17:54:11 +0900

    fix

--- 13.roman-to-integer.py
+++ 13.roman-to-integer.py
@@ -7,14 +7,18 @@
 # @lc code=start
 class Solution:
     def romanToInt(self, s: str) -> int:
+        """
+        time: O(len(s))
+        space: O(1)
+        """
         ans = 0
-        dict = {'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500, 'M' : 1000,}
+        dict_ = {'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500, 'M' : 1000,}
         for i in range(len(s)-1):
-            if dict[s[i]] < dict[s[i+1]]:
-                ans -= dict[s[i]]
+            if dict_[s[i]] < dict_[s[i+1]]:
+                ans -= dict_[s[i]]
             else:
-                ans += dict[s[i]]
-        ans += dict[s[len(s)-1]]
+                ans += dict_[s[i]]
+        ans += dict_[s[len(s)-1]]
         return ans
 # @lc code=end
 
