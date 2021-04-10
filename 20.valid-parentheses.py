fd905d79c085b5a51f3715802bff3f0ed52a86d5
kobakobashu <kobaya4@fuji.waseda.jp>
2021-04-10 18:23:00 +0900

    fix

--- 20.valid-parentheses.py
+++ 20.valid-parentheses.py
@@ -5,23 +5,24 @@
 #
 
 # @lc code=start
+from collections import deque
 class Solution:
     def isValid(self, s: str) -> bool:
         """
         time: O(len(s))
         space: O(len(s))
         """
-        stack = []
+        stack = deque()
         brackets_dict = {")" : "(" , "}" : "{" , "]" : "[", }
         for char in s:
             if char in brackets_dict.values():
                 stack.append(char)
             elif char in brackets_dict.keys():
-                if len(stack) == 0 or brackets_dict[char] != stack.pop():
+                if not stack or brackets_dict[char] != stack.pop():
                     return False
             else:
                 return False
-        return stack == []
+        return not stack
 
 # @lc code=end
 
