d942fce581c0aa0f29e9d07e0c06c3d1300ddb0e
kobakobashu <kobaya4@fuji.waseda.jp>
2021-04-18 20:03:12 +0900

    fix

--- 66.plus-one.py
+++ 66.plus-one.py
@@ -9,8 +9,7 @@ class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         """
         time: O(len(digits))
-        space: O(len(digits))
-        """
+        space: O(len(digits))       
         number_str = ""
         for digit in digits:
             number_str += str(digit)
@@ -19,6 +18,18 @@ class Solution:
         for digit in str(output_num):
             output.append(digit)
         return output
+        """
+
+        """
+        time: O(len(digits))
+        space: O(len(digits))  
+        """     
+        number_str = ""
+        digit_list = []
+        for digit in digits:
+            digit_list.append(str(digit))
+        output_num = int(''.join(digit_list)) + 1
+        return list(str(output_num))
         
 # @lc code=end
 
