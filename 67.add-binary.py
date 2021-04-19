db69102eb00017eb93fa7512259be6db494e4009
kobakobashu <kobaya4@fuji.waseda.jp>
2021-04-19 13:32:34 +0900

    fix

--- 67.add-binary.py
+++ 67.add-binary.py
@@ -8,10 +8,9 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
         """
-        time: O(1)
+        time: O(len(a)+len(b))
         space: O(1)
         """
-        output_10 = int(a,2) + int(b,2)
-        return bin(output_10).replace('0b', '')
+        return bin(int(a,2) + int(b,2)).replace('0b', '')
 # @lc code=end
 
