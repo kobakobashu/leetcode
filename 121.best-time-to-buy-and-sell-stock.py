5d38a87530ded561f2a3db9fa524acc6efbf3198
kobakobashu <kobaya4@fuji.waseda.jp>
2021-05-02 02:16:55 +0900

    best-time-to-buy-and-sell-stock

---
+++ 121.best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode id=121 lang=python3
+#
+# [121] Best Time to Buy and Sell Stock
+#
+
+# @lc code=start
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        """
+        time: O(len(prices))
+        space: O(len(prices))
+        """
+        if not prices:
+            return ""
+        min_price = prices[0]
+        profit = 0
+        for price in prices:
+            min_price = min(price, min_price)
+            profit = max(price - min_price, profit)
+        return profit
+        
+# @lc code=end
+
