44701a6953919c91c41ddf5a1c585fe938fb43a3
kobakobashu <kobaya4@fuji.waseda.jp>
2021-05-22 20:52:58 +0900

    add complexity

--- 242.valid-anagram.py
+++ 242.valid-anagram.py
@@ -7,11 +7,12 @@
 # @lc code=start
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        """
+        time: O(nlogn)   n : len(s)
+        space: O(len(n))
+        """
         sorted_s = ','.join(sorted(s))
         sorted_t = ','.join(sorted(t))
-        #if sorted_s == sorted_t:
-        #    return True
         return sorted_s == sorted_t
-
 # @lc code=end
 
