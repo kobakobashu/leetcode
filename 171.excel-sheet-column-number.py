ecfd9c691539b7eb9e57877328991975ba605229
kobakobashu <kobaya4@fuji.waseda.jp>
2021-05-23 18:14:08 +0900

    use chr

---
+++ 171.excel-sheet-column-number.py
@@ -0,0 +1,25 @@
+
+
+#
+# @lc app=leetcode id=171 lang=python3
+#
+# [171] Excel Sheet Column Number
+#
+
+# @lc code=start
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        """
+        time: O(len(columnTitle))
+        space: O(len(columnTitle))
+        """
+        ref = {}
+        for i in range(1, 27):
+            ref[chr(64 + i)] = i
+        columnTitle_list = list(columnTitle)
+        cur_num = 0
+        for idx, char in enumerate(columnTitle_list):
+            cur_num += ref[char] * (26 ** (len(columnTitle_list) - idx - 1))
+        return cur_num
+# @lc code=end
+
