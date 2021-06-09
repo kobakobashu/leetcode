ac81db73b5bc7db80c3e77d83af52f4d5b517edc
kobakobashu <kobaya4@fuji.waseda.jp>
2021-06-09 22:19:30 +0900

    add complexity

--- 409.longest-palindrome.py
+++ 409.longest-palindrome.py
@@ -9,6 +9,10 @@ import collections
 # @lc code=start
 class Solution:
     def longestPalindrome(self, s: str) -> int:
+        """
+        time: O(len(s))
+        space: O(len(s))
+        """
         s_counter = collections.Counter(s)
         palindrome_len = 0
         for num in s_counter.values():
